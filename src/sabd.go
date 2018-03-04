d514cdc5770bc8a1406c21b75de66f65cceddbe9
Jujhar Singh <jujhar@jujhar.com>
2018-03-04 22:08:06 +0000

    starting to work

--- src/sabd.go
+++ src/sabd.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/urfave/cli" // imports as package "cli"
 )
@@ -19,13 +18,22 @@ const dbName = "iGurbani.sqlite"
 const dbURL = "https://www.dropbox.com/sh/qazoft8own8u1na/AADXugSSY0IjqPw2qP1tkL3oa?dl=1" // TODO move to s3
 
 func main() {
-
+	var searchString, sabdId string;
 	app := cli.NewApp()
-	app.Name = "boom"
-	app.Usage = "make an explosive entrance"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("boom! I say!")
-		return nil
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "search, s",
+			Value:       "",
+			Usage:       "First letter search chars",
+			Destination: &searchString,
+		},
+		cli.StringFlag{
+			Name:        "display, d",
+			Value:       "",
+			Usage:       "which Sabd Id to render",
+			Destination: &sabdId,
+		},
 	}
 
 	err := app.Run(os.Args)
@@ -36,11 +44,11 @@ func main() {
 	dbPath := os.TempDir() + databasePath + dbName
 	log.Println("Checking " + dbPath + " to see if it exists or not")
 
-	/*
+	/**
 		does db hosting dir exist?
 		does db exist locally?
 		download db if not
-	*/
+	 */
 	fileStats, errStat := os.Stat(dbPath)
 	if errStat != nil {
 
@@ -59,24 +67,30 @@ func main() {
 		log.Printf("db alredy exists at %s %d bytes", dbPath, fileStats.Size)
 	}
 
-	searchString := "hjkkq"
+	/*
+		search
+	 */
+	if searchString != "" {
+		log.Printf("Searching for %s", searchString)
 
-	database, _ := sql.Open("sqlite3", dbPath)
-	rows, _ := database.Query("select _id,gurmukhi,english_ssk,transliteration from shabad where first_ltr_start like '" + stringToFirstLetterSearch(searchString) + "%' order by _id,ang_id,line_id")
+		database, _ := sql.Open("sqlite3", dbPath)
+		rows, _ := database.Query("select _id,gurmukhi,english_ssk,transliteration from shabad where first_ltr_start like '" + stringToFirstLetterSearch(searchString) + "%' order by _id,ang_id,line_id")
 
-	fmt.Println(stringToFirstLetterSearch("hjkk"))
+		var id int
+		var sabd, english, transliteration string
+		for rows.Next() {
+			rows.Scan(&id, &sabd, &english, &transliteration)
+			log.Printf(strconv.Itoa(id) + ": " + sabd + " " + english + " " + transliteration)
+		}
+	}
 
-	var id int
-	var sabd, english, transliteration string
-	for rows.Next() {
-
-		rows.Scan(&id, &sabd, &english, &transliteration)
-		log.Printf(strconv.Itoa(id) + ": " + sabd + " " + english + " " + transliteration)
+	if sabdId != "" {
+		//TODO render gurbani
 	}
 }
 
 /*
- TODO test
+ TODO
 */
 func stringToFirstLetterSearch(input string) (output string) {
 	//convert strings to rune array then to int arrray
