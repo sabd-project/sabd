42c9a291b5ebe0d46087bafd4a39b49baf8e1836
Jujhar Singh <jujhar@jujhar.com>
2018-03-04 18:41:18 +0000

    parking for the evening

--- src/sabd.go
+++ src/sabd.go
@@ -8,8 +8,10 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/urfave/cli" // imports as package "cli"
 )
 
 const databasePath = "/sabdDb/"
@@ -17,43 +19,68 @@ const dbName = "iGurbani.sqlite"
 const dbURL = "https://www.dropbox.com/sh/qazoft8own8u1na/AADXugSSY0IjqPw2qP1tkL3oa?dl=1" // TODO move to s3
 
 func main() {
-	l := log.New(os.Stdout, "", 0)
+
+	app := cli.NewApp()
+	app.Name = "boom"
+	app.Usage = "make an explosive entrance"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	dbPath := os.TempDir() + databasePath + dbName
-	l.Println("Checking " + dbPath + " to see if it exists or not")
+	log.Println("Checking " + dbPath + " to see if it exists or not")
+
 	/*
 		does db hosting dir exist?
 		does db exist locally?
 		download db if not
 	*/
-	_, err := os.Stat(dbPath)
-	if err != nil {
+	fileStats, errStat := os.Stat(dbPath)
+	if errStat != nil {
 
 		err2 := os.MkdirAll(os.TempDir()+databasePath, 0777)
 		if err2 != nil {
-			l.Fatal(err2)
+			log.Fatal(err2)
 		}
 
 		// now download file
-		l.Println("Downloading Gurbani DB from Internet...")
+		log.Println("Downloading Gurbani DB from Internet...")
 		err3 := downloadFile(dbPath, dbURL)
 		if err3 != nil {
-			l.Fatal(err3)
+			log.Fatal(err3)
 		}
+	} else {
+		log.Printf("db alredy exists at %s %d bytes", dbPath, fileStats.Size)
 	}
 
+	searchString := "hjkkq"
+
 	database, _ := sql.Open("sqlite3", dbPath)
-	// statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
-	// statement.Exec()
-	// statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
-	// statement.Exec("Nic", "Raboy")
-	rows, _ := database.Query("select _id,gurmukhi,english_ssk,transliteration from shabad where first_ltr_start like '107,107,107,107,107,107%' order by _id,ang_id,line_id")
+	rows, _ := database.Query("select _id,gurmukhi,english_ssk,transliteration from shabad where first_ltr_start like '" + stringToFirstLetterSearch(searchString) + "%' order by _id,ang_id,line_id")
+
+	fmt.Println(stringToFirstLetterSearch("hjkk"))
+
 	var id int
 	var sabd, english, transliteration string
 	for rows.Next() {
+
 		rows.Scan(&id, &sabd, &english, &transliteration)
-		fmt.Println(strconv.Itoa(id) + ": " + sabd + " " + english + " " + transliteration)
+		log.Printf(strconv.Itoa(id) + ": " + sabd + " " + english + " " + transliteration)
 	}
+}
+
+func stringToFirstLetterSearch(input string) (output string) {
+	//convert strings to rune array then to int arrray
+	runes := []int32([]rune(input))
+
+	// https://stackoverflow.com/questions/37532255/one-liner-to-transform-int-into-string/37533144
+	return strings.Trim(strings.Join(strings.Split(fmt.Sprint(runes), " "), ","), "[]")
 }
 
 /*
